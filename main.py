71a066fdd85dfca7b92f65a29b7d55a437e8b30a
Grzegorz134 <113019813+Grzegorz134@users.noreply.github.com>
2022-12-14 01:31:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+class Calculator:
+
+    def init(self):
+        pass
+
+    def sum(self, a, b):
+        return a + b
+
+    def raz(self, a, b):
+        return a - b
+
+    def proizv(self, a, b):
+        return a * b
+
+    def delenie(self, a, b):
+        return a / b
